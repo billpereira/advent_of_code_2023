f84344435a5454227587a0e857aa82fe4383e635
William Pereira <billpereira@MBP-de-William.home>
2023-12-01 12:15:00 +0100

    challenge 1 in python

--- day1.py
+++ day1.py
@@ -4,32 +4,33 @@ import re
 input_file = open("day1_input", "r")
 calibration_file = input_file.readlines()
 
-# Creating a table of conversion, keeping the word as with a hard replace it
-# will sometimes loose numbers if the words overlap i.e. eightwo
+# Creating a table of conversion for the spelled numbers
 convert_table = {
-    'one':'on1e',
-    'two':'tw2o',
-    'three':'thr3ee',
-    'four':'fo4ur',
-    'five':'fiv5e',
-    'six':'si6x',
-    'seven':'sev7en',
-    'eight':'eig8ht',
-    'nine':'ni9ne'
+    'one':'1',
+    'two':'2',
+    'three':'3',
+    'four':'4',
+    'five':'5',
+    'six':'6',
+    'seven':'7',
+    'eight':'8',
+    'nine':'9'
 }
 
 sum = 0
 
 def get_calibration_digits(calibration_line, current_sum):
-    digits_of_line = re.findall('[\d]',calibration_line)
-    first_digit = digits_of_line[0]
-    last_digit = digits_of_line[-1]
+    # Regex with lookahead adding the numbers as they are spelled to allow find them together with numeric digit
+    digits_of_line = re.findall('(?=(one|two|three|four|five|six|seven|eight|nine|\d))',calibration_line)
+    print(digits_of_line)
+    # Changing them using the convert table in case they are as spelled
+    first_digit = digits_of_line[0] if len(digits_of_line[0]) == 1 else convert_table[digits_of_line[0]]
+    last_digit = digits_of_line[-1] if len(digits_of_line[-1]) == 1 else convert_table[digits_of_line[-1]]
     calibration_value = first_digit+last_digit
     current_sum = current_sum + int(calibration_value)
     return current_sum
 
+# Loop through the input file
 for line in calibration_file:
-    for key,value in convert_table.items():
-        line = line.replace(key, value)
     sum = get_calibration_digits(line, sum)
     print(sum)
